6cdc529544ae0dd2bc369a5847e4aa04df94bcf2
gpaga <giuseppe.pagano@st.com>
2025-07-14 22:49:14 +0200

    Add new files for STM32MP1x

---
+++ DEVICETREE/CUSTOM_EXT_DTS_FOR_STM32MP135F-DK/optee/conf.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: BSD-2-Clause
+
+# MP13 boards
+flavor_dts_file-MYBOARD = stm32mp135f-myboard.dts
+flavorlist-MP13 += $(flavor_dts_file-MYBOARD)
+
+# MP15 boards
+
+# MP21 boards
+
+# MP25 boards
---
+++ FLASH_LAYOUT/stm32mp1/flash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+export PATH=$PATH:/opt/st/STM32Prog/bin/
+
+# STORAGE="emmc"
+STORAGE="sdcard"
+
+STM32_Programmer_CLI -c port=USB1 -w FLASH_LAYOUT/flash_layout_${STORAGE}.tsv
+
